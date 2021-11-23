fdc0175cf17ab4a1b1a8f7588ab996ab414b21a7
Dup4 <lyuzhi.pan@gmail.com>
2021-11-23 14:35:05 +0800

    fix: ignore SSH protocol banner

--- ssh_uptime/__main__.py
+++ ssh_uptime/__main__.py
@@ -24,6 +24,9 @@ if __name__ == '__main__':
             logger.info(
                 'Ping Test - Hostname:{}, Status:{}, Msg:{}'.format(machine.hostname, status, msg))
 
+            if msg == "Error reading SSH protocol banner":
+                status = Status.SUCCESS
+
             if status == Status.SUCCESS:
                 machine_status.is_survival = True
                 machine_status.last_survival_time = get_time()
